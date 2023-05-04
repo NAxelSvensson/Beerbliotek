6c660a8703d4c31e9103cb7771989b0ef825e195
Axel <n.axelsvensson@gmail.com>
2023-05-04 15:40:41 +0200

    Changed Type from int to string

--- BeerbliotekWebApplication/BeerbliotekWebApplication/Pages/Admin/AdminMenu.cshtml.cs
+++ BeerbliotekWebApplication/BeerbliotekWebApplication/Pages/Admin/AdminMenu.cshtml.cs
@@ -17,46 +17,6 @@ namespace BeerbliotekWebApplication.Pages.Admin
         public void OnGet()
         {
             ListBeers = databaseContext.Beers.ToList();
-            /*
-            try
-            {
-                string connectionString = "Data Source = localhost; Initial Catalog = Beerbliotek; Integrated Security = True";
-
-				using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string sql = "SELECT * FROM Beers";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                //save data into object
-                                Beer2 beerInfo = new Beer2();
-
-                                //id is of type string, but in the database it is of type int
-                                //so we add empty string to be able to convert the int into string
-                                beerInfo.Id = "" + reader.GetInt32(0);
-                                beerInfo.Name = reader.GetString(1);
-                                beerInfo.Alcohol = "" + reader.GetSqlSingle(2);
-								beerInfo.Price = "" + reader.GetSqlSingle(3);
-								beerInfo.Volume = "" + reader.GetInt32(4);
-								beerInfo.Type = "" + reader.GetInt32(5);
-								beerInfo.Country = reader.GetString(6);
-
-								listBeers.Add(beerInfo);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
-            */
         }
     }
 }
