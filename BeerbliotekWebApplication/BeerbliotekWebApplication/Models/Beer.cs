43ee801b64946f3a3f32f0bc6f8a4540cc3243e5
Axel Svensson <n.axelsvensson@gmail.com>
2023-05-09 13:55:25 +0200

    Changed in Beer from "float" to "double" Added some data to database if you don't have any

--- BeerbliotekWebApplication/BeerbliotekWebApplication/Models/Beer.cs
+++ BeerbliotekWebApplication/BeerbliotekWebApplication/Models/Beer.cs
@@ -8,13 +8,21 @@ namespace BeerbliotekWebApplication.Models
 		public int Id { get; set; }
 		[MaxLength(45)]
 		public string Name { get; set; }
-		public float Alcohol { get; set; }
-		public float Price { get; set; }
+		public double Alcohol { get; set; }
+		public double Price { get; set; }
 		public int Volume { get; set; }
 		[MaxLength(45)]
 		public string Type { get; set; }
 		[MaxLength(45)]
 		public string Country { get; set; }
-
+		public Beer(string name, double alcohol, double price, int volume, string type, string country)
+		{
+			Name = name;
+			Alcohol = alcohol;
+			Price = price;
+			Volume = volume;
+			Type = type;
+			Country = country;
+		}
 	}
 }
--- BeerbliotekWebApplication/BeerbliotekWebApplication/Pages/Index.cshtml.cs
+++ BeerbliotekWebApplication/BeerbliotekWebApplication/Pages/Index.cshtml.cs
@@ -6,10 +6,29 @@ namespace BeerbliotekWebApplication.Pages
 {
     public class IndexModel : PageModel
     {
+        DatabaseContext databaseContext;
+        public IndexModel(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
 
         public void OnGet()
         {
-            
+            if(databaseContext.Beers.ToList().Count == 0)
+            {
+                databaseContext.Beers.Add(new Beer("Staropramen", 5, 17.90, 330, "Ljus Lager", "Tjeckien"));
+                databaseContext.Beers.Add(new Beer("Guinness", 4.2, 25.50, 440, "Porter och Stout", "Irland"));
+                databaseContext.Beers.Add(new Beer("Mariestads Export", 5.3, 13.50, 330, "Ljus Lager", "Sverige"));
+                databaseContext.Beers.Add(new Beer("Kona Brewing", 4.4, 23.90, 355, "Ale", "USA"));
+                databaseContext.Beers.Add(new Beer("Corona Extra", 4.5, 19.90, 355, "Ljus Lager", "Mexiko"));
+                databaseContext.Beers.Add(new Beer("Brooklyn", 5.5, 17.90, 330, "Ale", "Sverige"));
+            }
+            if(databaseContext.Accounts.ToList().Count == 0)
+            {
+                Account adminAccount = new Account() { Username = "admin", Password = "admin123" };
+                databaseContext.Accounts.Add(adminAccount);
+            }
+            databaseContext.SaveChanges();
         }
     }
 }
